ac29c59f5b6e4d29447b2b9dc05356b1af652ba3
Manojkun1996 <manoj.bemt.14@gmail.com>
2023-01-25 14:47:35 +0100

    First Commit

---
+++ Pick_n_Place/Source Files/VAR_TAB.h
@@ -0,0 +1,26 @@
+Dim currentCanWeight As Single
+Dim currentRobotPosition As Position
+Dim currentCanRackPosition As Position
+Dim ScaleOpenDoors as Integer =50
+
+'===== Type I Variables =====
+Dim can_count as Integer = 50
+
+'===== Type F Variables =====
+'===== Type D Variables =====
+Dim weight as Double = 0
+Dim can_weight as Double = 7.2
+Dim tolerance as Double = 0.1
+'===== Type V Variables =====
+'===== Type P Variables =====
+Dim can_dim as Position
+can_dim.X = 2.1
+can_dim.Y = 59.1
+'===== Type J Variables =====
+'===== Type T Variables =====
+'===== Type S Variables =====
+'===== Tool =====
+'===== Work =====
+'===== Area =====
+'===== Exclusive area =====
+'Variable Declarations
